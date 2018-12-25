e566b2cdfba559736ec6c39c8714d9d848827f5e
StriveNuby <1477970338@qq.com>
2018-12-25 17:13:27 +0800

    first commit

---
+++ src/js/content-script.js
@@ -0,0 +1,116 @@
+// 获取节点
+var _$ = function(str) {
+  return document.querySelector(str);
+};
+
+// 查询 h 标签级别
+function queryLevel(h) {
+  return parseInt(h.tagName.replace("H", ""), 10);
+}
+
+// 渲染节点
+// h1 h2 h3 h3
+function renderChild(tree, ol, isChildNode) {
+  var len = tree.length,
+    arr = [];
+  if (len === 0) return false;
+  if (isChildNode) {
+    var newOl = document.createElement("ol");
+    ol.appendChild(newOl);
+    ol = newOl;
+  }
+  if (len === 1) {
+    ol.appendChild(addLi(tree[0]));
+    return false;
+  }
+  for (var i = 0; i < len; i++) {
+    if (arr[0] === undefined) {
+      arr.push(tree[i]); // h1
+    } else {
+      var oldLevel = (arr[0] && queryLevel(arr[0])) || 0,
+        level = queryLevel(tree[i]);
+      if (level > oldLevel) {
+        var node = null,
+          len1 = arr.length;
+        // 2 1
+        for (var j = 0; j < len1; j++) {
+          node = addLi(arr[j]);
+          ol.appendChild(node);
+        }
+        renderChild(tree.splice(len1, tree.length), node, true);
+        break;
+      } else {
+        arr.push(tree[i]);
+      }
+    }
+  }
+}
+
+// 添加 li
+function addLi(h) {
+  var li = document.createElement("li"),
+    aim = h.querySelector("a").getAttribute("href"),
+    a = document.createElement("a"),
+    text = h.textContent || "";
+  a.setAttribute("href", aim);
+  a.setAttribute("title", text);
+  a.innerText = text;
+  li.appendChild(a);
+  return li;
+}
+
+// 初始化
+function init(list) {
+  // nav
+  var nav = document.createElement("div");
+  nav.classList.add("markdown-nav-main");
+  // content
+  var content = document.createElement("div");
+  content.classList.add("content");
+  // title
+  var title = document.createElement("div");
+  title.classList.add("title");
+  var title_msg = document.createTextNode("目录: ");
+  title.appendChild(title_msg);
+  // toc
+  var toc = document.createElement("div");
+  toc.classList.add("toc");
+
+  var ol = document.createElement("ol");
+  var tree = [],
+    len = list.length;
+  for (var i = 0; i < len; i++) {
+    var header = list[i];
+    //   1 2 2 3 2
+    var level = queryLevel(header);
+    if (tree.length < 1) {
+      tree.push(header);
+    } else {
+      var oldLevel = queryLevel(tree[0]);
+      // 2 1
+      if (oldLevel >= level) {
+        renderChild(tree, ol);
+        tree = [];
+      }
+      tree.push(header);
+    }
+    if (i === len - 1 && tree.length !== 0) {
+      renderChild(tree, ol);
+    }
+  }
+  toc.appendChild(ol);
+  content.appendChild(title);
+  content.appendChild(toc);
+  nav.appendChild(content);
+  return nav;
+}
+
+setTimeout(function() {
+  var git_reg = /[https://github.com].*/,
+    _readme = _$("article");
+  if (git_reg.test(document.URL) && _readme) {
+    var data = _readme.querySelectorAll("h1,h2,h3,h4,h5,h6");
+    var div = init(data);
+    document.body.appendChild(div);
+  }
+}, 700);
