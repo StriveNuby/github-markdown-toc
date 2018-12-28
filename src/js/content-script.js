48b9cde93684c3c19e61b06e28f1672e58a52895
StriveNuby <1477970338@qq.com>
2018-12-28 17:29:01 +0800

    add test demo

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -11,6 +11,7 @@ function queryLevel(h) {
 // 渲染节点
 // h1 h2 h3 h3
 function renderChild(tree, ol, isChildNode) {
+  console.log(tree, ol, isChildNode);
   var len = tree.length,
     arr = [];
   if (len === 0) return false;
@@ -24,11 +25,15 @@ function renderChild(tree, ol, isChildNode) {
     return false;
   }
   for (var i = 0; i < len; i++) {
+    console.log("======");
+    console.log(arr[0]);
     if (arr[0] === undefined) {
       arr.push(tree[i]); // h1
     } else {
       var oldLevel = (arr[0] && queryLevel(arr[0])) || 0,
         level = queryLevel(tree[i]);
+      console.log(level, oldLevel);
+      console.log(arr);
       if (level > oldLevel) {
         var node = null,
           len1 = arr.length;
@@ -43,6 +48,14 @@ function renderChild(tree, ol, isChildNode) {
         arr.push(tree[i]);
       }
     }
+    // if (arr.length > 0 && i === len - 1) {
+    //   var node = null,
+    //     len1 = arr.length;
+    //   for (var j = 0; j < len1; j++) {
+    //     node = addLi(arr[j]);
+    //     ol.appendChild(node);
+    //   }
+    // }
   }
 }
 
@@ -108,7 +121,8 @@ function init(list) {
 setTimeout(function() {
   var git_reg = /[https://github.com].*/,
     _readme = _$("article");
-  if (git_reg.test(document.URL) && _readme) {
+  // if (git_reg.test(document.URL) && _readme) {
+  if (_readme) {
     var data = _readme.querySelectorAll("h1,h2,h3,h4,h5,h6");
     var div = init(data);
     document.body.appendChild(div);
